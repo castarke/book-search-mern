e2c3c3bb6ec453db24d684cf5adc32b9dd8fd29e
castarke <candler.starke@yahoo.com>
2023-06-26 00:53:17 -0400

    fixed error of writing mutations on query page and fixed the queriers.js. working on fixing other bugs relating on page not displaying

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import {useMutation} from '@apollo/client'
-import {ADD_USER } from '../utils/mutations';
+import {CREATE_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -12,7 +12,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [addUser, {error}] = useMutation(ADD_USER)
+  const [addUser, {error}] = useMutation(CREATE_USER)
 
   useEffect(() =>{
     if (error) {
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,61 +1,20 @@
-import {gql} from 'apollo/client';
+import {gql} from '@apollo/client';
 
-export const LOGIN_USER = gql`
-    mutation loginUser($email: String!, $password: String!) {
-        loginUser(email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
-    }
-`;
+export const GET_ME = gql`
 
-export const CREATE_USER = gql`
-    mutation createUser($username: String!, $email: String!, $password: String!) {
-        creaateUser(username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
+    me {
+      username
+      _id
+      email
+      bookCount
+      password
+      savedBooks {
+        bookId
+        description
+        image
+        link
+        title
+        authors
+      }
     }
-`;
-
-export const SAVE_BOOK = gql`
-    mutation saveBook($bookData: BookInput!) {
-        saveBook(bookData: $bookData) {
-            _id
-            username
-            email
-            bookCount
-            savedBooks {
-                bookId
-                authors
-                title
-                description
-                image
-                link
-            }
-        }
-    }
-`;
-
-export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId: ID!) {
-        _id
-        username
-        email
-        bookCount
-        savedBooks {
-            bookId
-            authors
-            title
-            description
-            image
-            link
-        }
-    }
-`;
+  `
