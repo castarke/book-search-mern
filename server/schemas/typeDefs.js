76add1c9706b92e935b3a4c2f6514d1fcff11ba6
castarke <candler.starke@yahoo.com>
2023-06-27 18:05:58 -0400

    fixed 400 error, now to debug the login feature

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -40,7 +40,7 @@ type Query {
 type Mutation {
   createUser( username: String!, email: String!, password: String!): Auth
   loginUser(email: String!, password: String!) : Auth
-  saveBook(book: BookInput!) : User
+  saveBook(bookData: BookInput!) : User
   removeBook( bookId: String!) : User
 }
 `;
