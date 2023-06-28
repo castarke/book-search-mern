61a039b28c84653e055fbf846998173de5f6d8ed
castarke <candler.starke@yahoo.com>
2023-06-27 23:39:10 -0400

    fixed a error that was throwing in the mutations when trying to delete book from saved.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -44,18 +44,20 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-    mutation removeBook($bookId: ID!) {
-        _id
-        username
-        email
-        bookCount
-        savedBooks {
-            bookId
-            authors
-            title
-            description
-            image
-            link
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                title
+                description
+                image
+                link
+            }
         }
     }
 `;
